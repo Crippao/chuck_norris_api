28a888b0bf44608524e53ed60c6933c92f94a11a
Crippao <cristina.paoloni@outlook.com>
2024-03-05 14:29:21 +0100

    add array of categories in .env and try it

--- bin/chuck_norris.dart
+++ bin/chuck_norris.dart
@@ -1,6 +1,27 @@
-import 'package:chuck_norris/chuck_norris.dart' as chuck_norris;
+// import 'package:chuck_norris/chuck_norris.dart' as chuck_norris;
 
-void main(List<String> arguments) async {
-  final quote = await chuck_norris.getQuote();
-  print(quote);
+// void main(List<String> arguments) async {
+//   final quote = await chuck_norris.getQuote();
+//   print(quote);
+// }
+
+import 'dart:io';
+
+import 'package:dotenv/dotenv.dart';
+
+void main() {
+  var env = DotEnv(includePlatformEnvironment: true)..load();
+
+  p('read all vars? ${env.isEveryDefined(['categories'])}');
+
+  p('categories=${env['categories']}');
+  p('your home directory is: ${env['HOME']}');
+
+  env.clear();
+  p('cleared!');
+
+  p('env has key categories? ${env.isDefined('categories')}');
+  p('your home directory is still: ${env['HOME']}');
 }
+
+p(String msg) => stdout.writeln(msg);
