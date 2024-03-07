698d56066b822564c3e0ead4e892952d25aed472
Cristina <109718308+Crippao@users.noreply.github.com>
2024-03-07 11:22:06 +0100

    add getCategoryQuoteByEnv method

--- lib/chuck_norris.dart
+++ lib/chuck_norris.dart
@@ -1,27 +1,64 @@
+import "dart:ffi";
+import "dart:io";
 import "models/quote.dart"; 
 import "dart:convert";
-import "package:http/http.dart" as http; 
+import "package:http/http.dart" as http;
+import 'package:dotenv/dotenv.dart';
 
-const url = "https://api.chucknorris.io/jokes/random";
-const urlCategory = "https://api.chucknorris.io/jokes/random?category={animal}";
+String? chooseCategoryByInput() {
+
+  stdout.writeln("1 - animal");
+  stdout.writeln("2 - career");
+  stdout.writeln("3 - celebrity");
+  stdout.writeln("4 - dev");
+  stdout.writeln("5 - explicit");
+  stdout.writeln("6 - fashion");
+  stdout.writeln("7 - food");
+  stdout.writeln("8 - history");
+  stdout.writeln("9 - money");
+  stdout.writeln("10 - movie");
+  stdout.writeln("11 - music");
+  stdout.writeln("12 - political");
+  stdout.writeln("13 - religion");
+  stdout.writeln("14 - science");
+  stdout.writeln("15 - sport");
+  stdout.writeln("16 - travel");
+  stdout.writeln('Di che categoria sarà la tua citazione? Inserisci un numero da 1 a 16');
+  final input = stdin.readLineSync();
+  stdout.writeln('You typed: $input'); //, the choosen category is $category[$input]');
+  return input;
+}
 
 Future<Quote> getQuote() async {
+  const url = "https://api.chucknorris.io/jokes/random";
   final urlParse = Uri.parse(url);
+  //stdout.writeln(urlParse);
   final response = await http.get(urlParse); 
+  //stdout.writeln(response);
 
   final Map<String, dynamic> data = json.decode(response.body); 
   
-  Quote res = Quote(quote:data['value'],id:data['id'],dateOfCreation:data['created_at']);
+  Quote res = Quote(quote:data['value'],id:data['id'],dateOfCreation:data['created_at'], category:"random");
   return res;
 }
 
-//cercare libreria per caricare file env
-Future<Quote> getCategoryQuote() async {
-  final urlParse = Uri.parse(url);
-  final response = await http.get(urlParse); 
+Future<Quote> getCategoryQuoteByEnv () async {
+  dynamic res;
+  var env = DotEnv(includePlatformEnvironment: true)..load();
+  if(env.isEveryDefined(['categories'])){
+    final category = env['categories'];
+    
+    //stdout.writeln(category);   
 
-  final Map<String, dynamic> data = json.decode(response.body); 
+    final urlCategory = "https://api.chucknorris.io/jokes/random?category=$category";
+
+    final urlParse = Uri.parse(urlCategory);
+    final response = await http.get(urlParse); 
+
+    final Map<String, dynamic> data = json.decode(response.body); 
+    //stdout.writeln(data);
   
-  Quote res = Quote(quote:data['value'],id:data['id'],dateOfCreation:data['created_at']);
-  return res;
+    res = Quote(quote:data['value'],id:data['id'],dateOfCreation:data['created_at'], category:category);
+  }
+  return res;  
 }
--- lib/models/quote.dart
+++ lib/models/quote.dart
@@ -2,12 +2,13 @@ class Quote {
   String quote;
   String id;
   String dateOfCreation;
+  String? category;
 
-  Quote({required this.quote, required this.id, required this.dateOfCreation});
+  Quote({required this.quote, required this.id, required this.dateOfCreation, this.category});
 
   @override
   String toString() {
-    String data = "This is your quote: $quote, its id is: $id and it was created in: $dateOfCreation";
+    String data = "This is your quote: $quote, its id is: $id, its category is $category and it was created in: $dateOfCreation";
     return data;
   }
 }
